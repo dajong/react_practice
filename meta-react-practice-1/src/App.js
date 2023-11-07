600621ae85e228610ff7b4ce0db753c6e3931b19
Daniel Jong <dajong@Daniels-MacBook-Air.local>
2023-11-07 20:23:00 +0000

    push meta react practice 1

---
+++ meta-react-practice-1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+// Default export is used to export a single class, function or primitive from a script file.
+// Named export is used to export several values from a script file.
+// e.g. export { myFunction }; or export const foo = Math.sqrt(2);
+export default App;
